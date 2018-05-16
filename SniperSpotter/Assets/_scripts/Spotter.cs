1b150063031fbba227a5821a2867dc80e97d6d93
Kara Thrash <dvdonato@gmail.com>
2018-05-16 13:25:56 -0400

    teleports

--- SniperSpotter/Assets/_scripts/Spotter.cs
+++ SniperSpotter/Assets/_scripts/Spotter.cs
@@ -8,7 +8,10 @@ public class Spotter : MonoBehaviour {
     public GameObject lastmarker;
     public GameObject groundlocation;
     public GameObject skylocation;
+    public GameObject fadeplane;
 
+    public GameObject bullet;
+    public GameObject windManager;
     // Use this for initialization
     void Start () {
 		
@@ -20,7 +23,8 @@ public class Spotter : MonoBehaviour {
         { Debug.Log("button 0"); PlaceMarker(); }
         if (Input.GetKeyUp("joystick button 1"))
         { Debug.Log("button 1"); EyeInTheSky(); }
-
+        if (Input.GetKeyUp("joystick button 3"))
+        { Debug.Log("button 1"); EyeInTheSky(); }
     }
 
     public void PlaceMarker()
@@ -41,9 +45,15 @@ public class Spotter : MonoBehaviour {
 
     public void EyeInTheSky()
     {
+        fadeplane.GetComponent<VRTeleportFade>().StartFade();
         if (transform.root.position == groundlocation.transform.position)
         { transform.root.position = skylocation.transform.position; transform.root.rotation = skylocation.transform.rotation; }
         else { transform.root.position = groundlocation.transform.position; transform.root.rotation = groundlocation.transform.rotation; }
     }
-
+    public void FireMissile()
+    {
+        GameObject clone = Instantiate(bullet, raycastObject.transform.position, raycastObject.transform.rotation) as GameObject;
+        clone.GetComponent<Bullet>().wind = windManager;
+        clone.GetComponent<Rigidbody>().AddForce(raycastObject.transform.forward * 20, ForceMode.Impulse);
+    }
 }
---
+++ SniperSpotter/Assets/_scripts/VRTeleportFade.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRTeleportFade : MonoBehaviour {
+    public Color colorStart = Color.red;
+    public Color colorEnd = Color.clear;
+    public Renderer fadematerial;
+    public bool fade;
+	// Use this for initialization
+	void Start () {
+        fadematerial = GetComponent<Renderer>();
+        colorEnd.a = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //fadematerial.material.color != colorEnd ||
+        if (fadematerial.material.color != colorEnd)
+        {
+            fadematerial.material.color = Color.Lerp(fadematerial.material.color, colorEnd, 0.2f);
+            // fadematerial.material.color.a -= 0.1f;
+        }
+     
+	}
+    public void StartFade()
+    {
+        fadematerial.material.color = colorStart;
+    }
+
+}
